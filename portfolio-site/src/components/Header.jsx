f71590c56132ccf0863eda1dd644d49227c67a75
Ian Hansson <ian713hansson@gmail.com>
2022-09-16 09:40:07 -0500

    starting to transfer

---
+++ portfolio-site/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+
+
+
+const Header = () => {
+    return (
+        <header>
+            <div className='header'>
+                <h1>Ian Hansson</h1>
+                <h1>Software Engineer</h1>
+            </div>
+            <nav className='nav'>
+                <Link className='link' to='/'>Home</Link>
+                
+                <Link className='link' to='/projects'>Projects</Link>
+            </nav>
+            <i class="fa-brands fa-linkedin"></i>
+        </header>
+    )
+}
+export default Header
