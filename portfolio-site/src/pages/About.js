7c47efd9a656c333f7ae7cf66fe975a1a6ec48f5
Ian Hansson <ian713hansson@gmail.com>
2022-09-16 09:41:11 -0500

    adding pages

---
+++ portfolio-site/src/pages/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+    return (
+        <div className="about_title">
+            <p className="about_blurb">
+            I am a software developer with a passion for design who is a natural leader with a proven track record of motivating personnel to perform at a high quality of standards. I place respect and understanding of others right on par with focus and efficient task completion. Never shying away from challenges I navigate complicated tasks and unpredictability with an ability to remain patient while using critical thinking in the moment.
+            </p>
+            
+            
+        </div>
+    )
+}
+
+export default About 
---
+++ portfolio-site/src/pages/Projects.js
@@ -0,0 +1,10 @@
+const Projects = () => {
+    return (
+        <div className="projects">
+            <h1>Where projects will be</h1>
+            
+        </div>
+    )
+}
+
+export default Projects
