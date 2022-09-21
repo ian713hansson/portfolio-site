bf0688654db0787849aba547d00a091500e5e7e3
Ian Hansson <ian713hansson@gmail.com>
2022-09-21 18:31:06 -0500

    removed projects not yet deployed

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,16 +12,7 @@ const Projects = () => {
                 </a>
                 <h4>Click Image to check out the site</h4>
             </div>
-            <div className='project-layout'>
-                <a href='https://github.com/ian713hansson/CelestialVibrationUnit'>
-                    <h1 className='project-title'>Celestial Vibration Unit</h1>
-                </a>
-                <p className='project-description'>My first Full Stack app is a love letter to my modular synthesizer, where you can explore different modules info and a place to listen to music created using it.</p>
-                {/* <a href='https://80s_metal_trivia.surge.sh/'> */}
-                {/* <img className='project-img' src="https://i.imgur.com/Fm7lnUg.jpg"/> */}
-                {/* </a> */}
-                <h4>As this site is not deployed yet, please check out this walkthrough</h4>
-            </div>
+            
             <div className='project-layout'>
                 <a href='https://github.com/steves3142/dynamic_polling_backend'>
                     <h1 className='project-title'>DynaSoar Polling</h1>
@@ -32,16 +23,7 @@ const Projects = () => {
                 </a>
                 <h4>Click Image to check out the site</h4>
             </div>
-            <div className='project-layout'>
-                <a href='https://github.com/ian713hansson/preSent'>
-                    <h1 className='project-title'>preSent</h1>
-                </a>
-                <p className='project-description'>My capstone project for my Software Engineering Immersive, is a meditation focused app. Intended as a space to relax with ambient music, guided meditations and relaxing visuals.</p>
-                <a href='https://dynasoar.herokuapp.com/'>
-                <img className='project-img' src="https://i.imgur.com/uekhoq3.png"/>
-                </a>
-                <h4>Click Image to check out a the site</h4>
-            </div>
+            
         </div>
     )
 }
